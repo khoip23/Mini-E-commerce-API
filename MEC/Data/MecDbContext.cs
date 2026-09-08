4a13d3ab3ee4bede6ba571255637f51868b608ec
khoi <khoimap2307@gmail.com>
2026-09-08 14:39:36 +0700

    MEC-7 Create Entity Prod + Category and Connect DB

---
+++ MEC/Data/MecDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MEC.Models;
+using Microsoft.EntityFrameworkCore;
+namespace MEC.Data
+{
+    public class MecDbContext : DbContext
+    {
+        public MecDbContext(DbContextOptions<MecDbContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+    }
+}
